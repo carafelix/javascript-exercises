ca8d8228da5da87362d70a1f96b705b7ba018d5a
carafelix <heroprotagonist32@gmail.com>
2023-06-22 12:58:36 -0400

    First test pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, num) {
 
+    const numArray = arr;
+
+    for (i = 0; i < numArray.length; i++) {
+        if (numArray[i] === num) {
+            numArray.splice(i, 1)
+        }
+    } return numArray
 };
 
 // Do not edit below this line
