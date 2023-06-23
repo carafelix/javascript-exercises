43ff95761a285a1f299d0461018ab1aa167236af
carafelix <heroprotagonist32@gmail.com>
2023-06-22 21:09:17 -0400

    comments

--- 05_sumAll/solution/sumAll-solution.js
+++ 05_sumAll/solution/sumAll-solution.js
@@ -1,8 +1,9 @@
 const sumAll = function (min, max) {
-  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR"; // much more elegant than my solution. works the same. 
+  //I tried doing min !== Number or min == !Number but that didn't work. Now I know that Number obj can be used auxiliarly
   if (min < 0 || max < 0) return "ERROR";
   if (min > max) {
-    const temp = min;
+    const temp = min; // Dancing! much shorter and easier  
     min = max;
     max = temp;
   }
