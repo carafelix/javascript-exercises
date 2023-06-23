77902eb5959e4e905f13e339ea8c38e333d51886
carafelix <heroprotagonist32@gmail.com>
2023-06-22 20:55:52 -0400

    Solve with typeof, forgot commits

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num, numi) {
 
+    const min = Math.min(num,numi);
+    const max = Math.max(num,numi);
+
+        if (num < 0 || numi < 0) {
+            return 'ERROR'
+        } else if ((typeof num !== "number") || (typeof numi !== "number")) {
+            return 'ERROR'
+        } else {
+            let sum = 0;
+
+            for(let i = min; i <= max; i++) {
+                sum += i;
+            }
+            return sum;
+        }
+    
 };
 
 // Do not edit below this line
